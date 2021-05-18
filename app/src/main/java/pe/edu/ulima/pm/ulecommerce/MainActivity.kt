bbd5dfec2f11262920d5d947353d84e0e569caeb
Hernan <hernan@jsatch.com>
2021-05-18 12:35:20 -0500

    Adding toolbar & drawerlayout

--- app/src/main/java/pe/edu/ulima/pm/ulecommerce/MainActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/ulecommerce/MainActivity.kt
@@ -1,59 +1,96 @@
 package pe.edu.ulima.pm.ulecommerce
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import com.google.android.material.navigation.NavigationView
 import pe.edu.ulima.pm.ulecommerce.fragments.AccountFragment
 import pe.edu.ulima.pm.ulecommerce.fragments.OnBottomBarMenuSelected
 import pe.edu.ulima.pm.ulecommerce.fragments.ProductsFragment
 import pe.edu.ulima.pm.ulecommerce.views.OnFaceClickListener
 import pe.edu.ulima.pm.ulecommerce.views.ULFaceView
 
-class MainActivity : AppCompatActivity(), OnBottomBarMenuSelected{
+class MainActivity : AppCompatActivity(){
     var username : String? = null
+    var dlaMain : DrawerLayout? = null
+    var fragments : ArrayList<Fragment> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar = findViewById<Toolbar>(R.id.tbaMain)
+        setSupportActionBar(toolbar)
+
+        // Setear actionbar
+        supportActionBar?.setHomeAsUpIndicator(android.R.drawable.btn_star)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //Crear fragments
         username = intent.getStringExtra("USERNAME")
 
         val bundle : Bundle = Bundle()
         bundle.putString("USERNAME", username)
 
-        val fragment = AccountFragment()
-        fragment.arguments = bundle // seteamos argumentos al fragment
+        fragments.add(AccountFragment())
+        fragments.add(ProductsFragment())
 
+        fragments[0].arguments = bundle // Seteamos parametros a AccountFragment
+
+        // Configurar nuestra navegacion
+        val nviMain = findViewById<NavigationView>(R.id.nviMain)
+        dlaMain = findViewById(R.id.dlaMain)
+
+        nviMain.setNavigationItemSelectedListener { item : MenuItem ->
+            item.setChecked(true)
+            val ft = supportFragmentManager.beginTransaction()
+            if (item.itemId == R.id.mnuAccount) {
+                // Abrimos fragment AccountFragment
+                ft.replace(R.id.flaContent, fragments[0])
+            }else if (item.itemId == R.id.mnuProducts) {
+                // Abrimos fragment ProductsFragment
+                ft.replace(R.id.flaContent, fragments[1])
+            }
+            ft.addToBackStack(null)
+            ft.commit()
+            dlaMain!!.closeDrawers()
+            true
+        }
+
+        // Agregamos fragment por defecto
         val ft = supportFragmentManager.beginTransaction()
-        ft.add(R.id.flaContent, fragment)
+        ft.add(R.id.flaContent, fragments[0])
         ft.addToBackStack(null)
         ft.commit()
 
     }
 
-    override fun onMenuSelected(menuType: String) {
-        var fragment : Fragment? = null
-        if (menuType == "Account") {
-            val bundle : Bundle = Bundle()
-            bundle.putString("USERNAME", username)
-            fragment = AccountFragment()
-            fragment.arguments = bundle
-        }else if (menuType == "Products") {
-            fragment = ProductsFragment()
-        }
-
-        if (fragment != null) {
-            val ft = supportFragmentManager.beginTransaction()
-            ft.replace(R.id.flaContent, fragment)
-            ft.addToBackStack(null)
-            ft.commit()
-        }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
-//    override fun onClick(view : ULFaceView) {
-//        Log.i("MainActivity", "Se hizo click")
-//        view.altoBoca = view.altoBoca?.plus(100f)
-//    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            dlaMain!!.openDrawer(GravityCompat.START)
+        }else if (item.itemId == R.id.mnuProductsActivity) {
+            startActivity(Intent(this, ProductsActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        dlaMain!!.closeDrawers()
+    }
+
 
 }
--- app/src/main/java/pe/edu/ulima/pm/ulecommerce/fragments/ProductsFragment.kt
+++ app/src/main/java/pe/edu/ulima/pm/ulecommerce/fragments/ProductsFragment.kt
@@ -28,5 +28,4 @@ class ProductsFragment : Fragment(){
             startActivity(intent)
         }
     }
-
 }
